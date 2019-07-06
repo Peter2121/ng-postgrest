c3fcbc22bbd4371cf43f0d268b7fb8a89626631e
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2019-07-06 18:53:47 +0300

    db test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,4 +33,58 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('#ng-postgrest').textContent).toBeTruthy();
   }));
+
+});
+
+describe('db', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, HttpClientTestingModule],
+      providers: [PostgrestServiceService],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should select the newly added db', async() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const url1 = { url: 'https://localhost/1', auth: 'MyToken_1' };
+    const url2 = { url: 'https://localhost/2', auth: 'MyToken_2' };
+    app.addUrl(url1);
+    app.addUrl(url2);
+    expect(app.currentUrl).toEqual(url2);
+  });
+
+  it('should save new db to local storage', async() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const url = { url: 'https://localhost', auth: 'MyToken' };
+    app.addUrl(url);
+    expect(window.localStorage.getItem('databaseUrls')).toEqual(JSON.stringify([url]));
+  });
+
+  it('should delete db from local storage', async() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const url1 = { url: 'https://localhost/1', auth: 'MyToken_1' };
+    const url2 = { url: 'https://localhost/2', auth: 'MyToken_2' };
+    app.addUrl(url1);
+    app.addUrl(url2);
+    app.deleteDB(); // deletes the selected db, last one added
+    expect(window.localStorage.getItem('databaseUrls')).toEqual(JSON.stringify([url1]));
+  });
+
+  it('should delete all db from local storage', async() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const url1 = { url: 'https://localhost/1', auth: 'MyToken_1' };
+    const url2 = { url: 'https://localhost/2', auth: 'MyToken_2' };
+    app.addUrl(url1);
+    app.addUrl(url2);
+    app.deleteAllDB();
+    expect(window.localStorage.getItem('databaseUrls')).toEqual(JSON.stringify([]));
+  });
+
 });
